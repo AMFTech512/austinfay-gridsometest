4c16406fbbc341e697adbb4b15b34e7f081ca790
Austin <amftech512@gmail.com>
2020-06-02 13:30:05 -0600

    updated templates/BlogPost.vue

--- src/pages/blog/Index.vue
+++ src/pages/blog/Index.vue
@@ -2,8 +2,8 @@
     <Layout>
         <h1>Blog Posts</h1>
         <ul>
-            <li v-for="blogPost in $static.posts.edges" :key="blogPost.id">
-                <g-link :to="`/blog/${blogPost.node.id}`">{{ blogPost.node.title }}</g-link>
+            <li v-for="blogPost in $static.posts.edges" :key="blogPost.node.id">
+                <g-link :to="blogPost.node.path">{{ blogPost.node.title }}</g-link>
             </li>
         </ul>
     </Layout>
@@ -15,6 +15,7 @@ query{
     edges{
       node{
         id
+        path
         title
         description
         content
